95cfe3e5ae9b557f0399ff795638d9c7ad18ef3a
˜Jhonata <jhonbonadio@gmail.com>
2023-06-16 17:27:07 -0300

    tema

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,10 +1,17 @@
 export default {
   colors: {
     shape: '#FFFFFF',
-    title: '#FFFFFF',
-    text: '#EEEEEE',
-    border: '#555',
-    background: '#222222',
+    title: '#f8f8f2',
+    text: '#f8f8f2',
+    comment: '#6272a4',
+    border: '#44475a',
+    background: '#282a36',
+    green: '#50fa7b',
+    red: '#ff5555',
+    yellow: '#f1fa8c',
+    purple: '#bd93f9',
+    orange: '#ffb86c',
+    cyan: '#8be9fd',
   },
 
   fonts: {
