5ce4a334fba74ef3cbf90b43753d9a179678c48c
Jhonata <jhonbonadio@gmail.com>
2023-06-28 12:07:45 -0300

    >

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -1,52 +1,28 @@
 import {Feather} from '@expo/vector-icons';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import * as React from 'react';
-import {View} from 'react-native';
 import {useTheme} from 'styled-components';
 
-import {SignIn} from '../screens/Authentication/Stack/SignIn';
 import {Home} from '../screens/Autheticated/Tab/Home';
 
-const {Navigator, Screen} = createMaterialBottomTabNavigator();
+const {Navigator, Screen} = createBottomTabNavigator();
 
 export function TabRoutes() {
   const theme = useTheme();
   return (
     <Navigator
-      activeColor={theme.colors.primary}
-      inactiveColor={theme.colors.comment}
-      sceneAnimationEnabled
-      sceneAnimationType="shifting"
-      labeled={false}
-      screenOptions={{}}
-      barStyle={{
-        backgroundColor: theme.colors.shape,
-        borderTopWidth: 1,
-        borderTopColor: theme.colors.border,
-        paddingBottom: 0,
-      }}>
-      <Screen
-        name="Initial"
-        component={Home}
-        options={{
-          tabBarIcon: ({color}) => (
-            <View>
-              <Feather name="home" size={24} color={color} />
-            </View>
-          ),
-        }}
-      />
-      <Screen
-        name="Teste"
-        component={SignIn}
-        options={{
-          tabBarIcon: ({color}) => (
-            <View>
-              <Feather name="home" size={24} color={color} />
-            </View>
-          ),
-        }}
-      />
+      screenOptions={({route}) => ({
+        tabBarActiveTintColor: theme.colors.primary,
+        tabBarInactiveTintColor: theme.colors.comment,
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarIcon: ({color}) => {
+          if (route.name === 'Initial') {
+            return <Feather name="home" size={24} color={color} />;
+          }
+        },
+      })}>
+      <Screen name="Initial" component={Home} />
     </Navigator>
   );
 }
